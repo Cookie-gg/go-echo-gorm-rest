babba5e8ea338bcb172017672322222273ffcf59
cookie_gg <cookie.gg.dev@gmail.com>
2023-01-06 04:04:24 +0900

    set param

--- models/user.go
+++ models/user.go
@@ -3,7 +3,7 @@ package models
 import "time"
 
 type User struct {
-	ID        uint      `json:"id"`
+	ID        uint      `json:"id" param:"id"`
 	Name      string    `json:"name"`
 	CreatedAt time.Time `json:"created_at"`
 	UpdatedAt time.Time `json:"updated_at"`
